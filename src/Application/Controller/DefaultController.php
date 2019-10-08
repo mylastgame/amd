f996087b01cbcea47045f42152f9569764bceecc
alexkim <mylastgame@gmail.com>
2019-10-08 22:58:09 +0300

    init commit

---
+++ src/Application/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Application\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    public function index()
+    {
+        return new Response('It works');
+    }
+}
